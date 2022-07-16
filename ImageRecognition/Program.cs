92eb5b46a0edd2bedcbd5fe911556d53babb2153
Mandar1jn <Marijnkneppers@gmail.com>
2022-07-16 16:37:52 +0200

    Trained model and basic program

--- ImageRecognition/Program.cs
+++ ImageRecognition/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter path to image: ");
+
+            string path = Console.ReadLine();
+
+            var sampleData = new ImageModel.ModelInput()
+            {
+                ImageSource = path,
+            };
+
+            var results = ImageModel.Predict(sampleData);
+
+            Console.WriteLine($"This image contains a {((results.Prediction == "cars") ? "car" : "wolf")}\n");
+
+            Console.WriteLine("Certainty:");
+
+            Console.WriteLine($"\tCar: {results.Score[0] * 100}%");
+            Console.WriteLine($"\tWolf: {results.Score[1] * 100}%");
         }
     }
 }
