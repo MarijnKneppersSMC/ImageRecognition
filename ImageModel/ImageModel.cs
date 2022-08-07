195cd55c23ec72f27879e97d268714303ef141f4
Mandar1jn <Marijnkneppers@gmail.com>
2022-08-07 19:14:20 +0200

    Created a model and console application

---
+++ ImageModel/ImageModel.cs
@@ -0,0 +1,60 @@
+﻿using static ImageModel.ImageModel;
+
+namespace ImageModel
+{
+    public partial class ImageModelHandler
+    {
+
+        public static PredictionResult PredictFile(string path)
+        {
+
+            string fullPath = Path.GetFullPath(path);
+
+            if(!File.Exists(fullPath))
+            {
+                return PredictionResult.FAILURE;
+            }
+
+            byte[] imageData = File.ReadAllBytes(fullPath);
+
+            ModelInput modelInput = new ModelInput()
+            {
+                ImageSource = imageData
+            };
+
+            ModelOutput output = Predict(modelInput);
+
+            return (PredictionResult)((output.PredictedLabel == "wolves")? PredictionResult.WOLF : PredictionResult.CAR);
+        }
+
+        public static List<ValueTuple<string, PredictionResult>> PredictDirectory(string path)
+        {
+            List<ValueTuple<string, PredictionResult>> results = new();
+
+            string fullPath = Path.GetFullPath(path);
+
+            if(!Directory.Exists(fullPath))
+            {
+                results.Add((fullPath, PredictionResult.FAILURE));
+                return results;
+            }
+
+            string[] files = Directory.GetFiles(fullPath);
+
+            for(int i = 0; i < files.Length; i++)
+            {
+                results.Add((files[i], PredictFile(files[i])));
+            }
+
+            return results;
+        }
+    }
+
+    public enum PredictionResult : int
+    {
+        FAILURE = -1,
+        CAR = 0,
+        WOLF = 1
+        
+    }
+}
---
+++ ImageRecognitionConsole/Program.cs
@@ -0,0 +1,76 @@
+﻿using static ImageModel.ImageModelHandler;
+using ImageModel;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ImagerecognitionConsole
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            List<ValueTuple<string, PredictionResult>> results = new();
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No command line arguments given. Please provide a file or folder path.");
+                string path = Console.ReadLine();
+                Predict(path);
+
+                results = Predict(path);
+            }
+            else
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    List<ValueTuple<string, PredictionResult>> output = Predict(args[i]);
+
+                    for(int j = 0; j < output.Count; j++)
+                    {
+                        results.Add(output[j]);
+                    }
+                }
+            }
+
+            Console.Clear();
+
+            Console.WriteLine(results.Count);
+
+            for(int i = 0; i < results.Count; i++)
+            {
+                Console.WriteLine($"{results[i].Item1}: {((int)results[i].Item2)}");
+            }
+
+        }
+
+        static List<ValueTuple<string, PredictionResult>> Predict(string path)
+        {
+            List<ValueTuple<string, PredictionResult>> output = new();
+
+            string fullPath = Path.GetFullPath(path);
+
+            if (File.Exists(fullPath))
+            {
+                PredictionResult result = PredictFile(fullPath);
+                output.Add((fullPath, result));
+            }
+            else if (Directory.Exists(fullPath))
+            {
+                List<ValueTuple<string, PredictionResult>> results = PredictDirectory(fullPath);
+
+                for (int i = 0; i < results.Count; i++)
+                {
+                    output.Add(results[i]);
+                }
+            }
+            else
+            {
+                output.Add((fullPath, PredictionResult.FAILURE));
+            }
+
+            return output;
+        }
+
+    }
+}
